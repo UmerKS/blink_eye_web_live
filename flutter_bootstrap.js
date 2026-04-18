623e2e5c5246e30f542461447ed7e98508d0e5b9
UmerKS <umerkhalid054@gmail.com>
2026-04-19 04:47:27 +0500

    update UI

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3152246479" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "824145909" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
